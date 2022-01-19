3dd2dba390b346a63798769a555594f808031742
amsgn-iscteiul <amsgn@iscte-iul.pt>
2022-01-19 23:36:17 +0000

    Added coroutine to button controller that periodically gets room status

--- Assets/Scripts/Eskills/Ui/ButtonsController.cs
+++ Assets/Scripts/Eskills/Ui/ButtonsController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using Eskills.Service.Repository;
 using UnityEngine;
 using UnityEngine.UI;
@@ -11,7 +12,7 @@ namespace Eskills.Ui
         [SerializeField] private Text sessionText;
         [SerializeField] private Text scoreText;
         [SerializeField] private Dropdown playerStatus;
-
+        private Boolean periodicUpdate = true;
         public void OnGetRoomInfoClick()
         {
             service.GetRoomInfo(sessionText.text, room => Debug.Log("ButtonsController: " + room.roomId),
@@ -26,6 +27,34 @@ namespace Eskills.Ui
             service.SetScore(sessionText.text, status, int.Parse(scoreText.text),
                 room => Debug.Log("ButtonsController: " + room.roomId),
                 error => Debug.Log("ButtonsController: " + error.Message));
+        }
+	    public void OnGetPeriodicUpdates()
+        {
+            var coroutine = GetPeriodicRoomInfoUpdates(sessionText.text,Success,Error);
+            StartCoroutine(coroutine);
+        }
+
+        private IEnumerator GetPeriodicRoomInfoUpdates(string session, Action<RoomData> success, Action<EskillsError> error)
+        {
+            while (periodicUpdate)
+            {
+                service.GetRoomInfo(session,
+                success,error);
+                Debug.Log("PERIODIC_SCORE_UPDATE");
+                yield return new WaitForSeconds(5.0f);
+            }
+        }
+        private void Success(RoomData room){
+            Debug.Log(room.status);
+            Debug.Log(room.roomId);
+            Debug.Log(room.players);
+        }
+        private void Error(EskillsError error){
+            Debug.Log(error);
+        }
+        public void cancelPeriodicUpdate()
+        {
+            periodicUpdate = false;
         }
     }
 }
